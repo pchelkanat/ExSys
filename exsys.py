59040f9980e09239dd601ff4e4347d9ead5e384e
pchelkanat <pchelkanat@mail.ru>
2019-03-22 02:00:57 +0300

    изменения и исправления рабочая версия

--- exsys.py
+++ exsys.py
@@ -8,15 +8,11 @@ class Rule():
     def __init__(self, name, priority, conditions, acts):
         self.name = name
         self.priority = priority
-        self.conditions = conditions
-        self.acts = acts
+        self.conditions = conditions  # if
+        self.acts = acts  # then
 
     def __str__(self):
         return "%s, %s, %s, %s" % (self.name, self.priority, self.conditions, self.acts)
-
-    def add_rule(self):
-        name, priority, conditions = input().splitlines()
-        return Rule(name, priority, conditions)
 
     def totxt(self):
         data = fromtxt(filepath)
@@ -25,6 +21,15 @@ class Rule():
             file.write(json.dumps(data, ensure_ascii=False))
 
         return True
+
+
+class Fact():
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+
+    def __str__(self):
+        return "%s: %s" % (self.key, self.value)
 
 
 def fromtxt(filepath):
@@ -37,32 +42,41 @@ def fromtxt(filepath):
     return data
 
 
-def create_my_rules():
-    templvl_normal = Rule("Определение нормального уровня температуры", 10, (35, 37.1),
-                          json.dumps({"температура": "нормальная"}, ensure_ascii=False))
-    templvl_medium = Rule("Определение среднего уровня температуры", 10, (37.1, 38.3),
-                          json.dumps({"температура": "средняя"}, ensure_ascii=False))
-    templvl_high = Rule("Определение высокого уровня температуры", 10, (38.3, 42),
-                        json.dumps({"температура": "высокая"}, ensure_ascii=False))
-    diag_healthy = Rule("Определение здорового диагноза", 10,
-                        json.dumps({"начало": "нет", "температура": "нормальная"}, ensure_ascii=False),
-                        json.dumps({"диагноз": "здоров"}, ensure_ascii=False))
-    diag_orz = Rule("Определение ОРЗ", 10,
-                    json.dumps({"начало": "постепенное", "температура": "средняя"}, ensure_ascii=False),
-                    json.dumps({"диагноз": "ОРЗ"}, ensure_ascii=False))
-    diag_flu = Rule("Определение гриппа", 10,
-                    json.dumps({"начало": "лихорадочное", "температура": "высокая"}, ensure_ascii=False),
-                    json.dumps({"диагноз": "грипп"}, ensure_ascii=False))
-    diag_undedefine = Rule("Определение неопределенного диагноза", 9, None,
-                           json.dumps({"диагноз": "не определен"}, ensure_ascii=False))
+def to_code(string):
+    code = "(" + string + ")"
+    code = code.replace(",", ") and (")
+    code = code.replace(";", " or ")
+    return code
 
-    templvl_normal.totxt()
-    templvl_medium.totxt()
-    templvl_high.totxt()
-    diag_undedefine.totxt()
-    diag_healthy.totxt()
-    diag_orz.totxt()
-    diag_flu.totxt()
+
+def create_my_rules():
+    rules = []
+    rules.append(Rule("Определение нормального уровня температуры", 10,
+                      "$температура >= 35, $температура < 37.1",
+                      json.dumps({"ур_темп": "нормальная"}, ensure_ascii=False)))
+    rules.append(Rule("Определение среднего уровня температуры", 10,
+                      "$температура >= 37.1, $температура < 38.3",
+                      json.dumps({"ур_темп": "средняя"}, ensure_ascii=False)))
+    rules.append(Rule("Определение высокого уровня температуры", 10,
+                      "$температура >= 38.3, $температура <= 42",
+                      json.dumps({"ур_темп": "высокая"}, ensure_ascii=False)))
+    rules.append(Rule("Определение здорового диагноза", 10,
+                      "$ур_темп == 'нормальная'",
+                      json.dumps({"диагноз": "здоров"}, ensure_ascii=False)))
+    rules.append(Rule("Определение ОРЗ", 10,
+                      "$начало == 'постепенное', $ур_темп == 'средняя'",
+                      json.dumps({"диагноз": "ОРЗ"}, ensure_ascii=False)))
+    rules.append(Rule("Определение гриппа2", 10,
+                      "$начало == 'лихорадочное', $ур_темп == 'высокая'",
+                      json.dumps({"предиаг": "грипп2"}, ensure_ascii=False)))
+    rules.append(Rule("Определение гриппа", 10,
+                      "$предиаг == 'грипп2'",
+                      json.dumps({"диагноз": "грипп3"}, ensure_ascii=False)))
+    rules.append(Rule("Определение неопределенного диагноза", 9, "",
+                      json.dumps({"диагноз": "не определен"}, ensure_ascii=False)))
+
+    for rule in rules:
+        rule.totxt()
 
 
 def my_rules():
@@ -75,59 +89,92 @@ def my_rules():
     return arr_rules
 
 
-def solving(my_start, my_temp):
+def input_data():
+    facts = []
+    while True:
+        print("Введите факт в формате ключ-значение с каждой новой строки")
+        key, value = input().split()
+        if value.isdecimal():
+            value = int(value)
+
+        facts.append(Fact(key, value))
+
+        print("Вы закончили?\n1. Да\n2. Нет")
+        answer = int(input())
+        if answer == 1:
+            return facts
+        else:
+            continue
+
+
+def solving(facts):
     arr_rules = my_rules()
-    my_templvl = ""
-    diag = ""
-    output = "\n#################\nINPUT: начало %s, температура %s\n____________________" % (my_start, my_temp)
+
+    output = "\n###############\nСписок правил:\n"
+    for rule in arr_rules:
+        output += str(rule) + "\n"
+
     for i in range(len(arr_rules)):  # проход по каждому правилу
-        # print("Итерация %s:\n проход правила: %s" % (i, arr_rules[i]))
+        fact_keys = [fact.key for fact in facts]  # ключи, которые есть. БУДУТ ДОПОЛНЯТЬСЯ
+        fact_values = [fact.value for fact in facts]
+        #print(fact_keys, fact_values)
         output += "\nИтерация %s:\n проход правила: %s\n" % (i, arr_rules[i])
+        act = json.loads(arr_rules[i].acts)  # подгужаем действия из одного правила
+        act_as_fact = ""
+        for item in act:
+            act_as_fact = Fact(item, act[item])  # представление действия, как факт
+        # [print(fact) for fact in act_as_fact]
+        if act_as_fact.key in fact_keys:  # если факт-действие уже есть, то прожолжаем
+            output += " пропускаем, так как факт уже имеется\n"
+            continue
+        else:
+            condition = arr_rules[i].conditions  # иначе, подгружаем условия
+            # print(to_code(condition))
+            if len(condition) == 0:
+                facts.append(Fact(act_as_fact.key, act_as_fact.value))  # если условие пустое, то добавляем этот факт
+                output += " правило удовлетворяет\n"
+            else:
+                for j in range(len(facts)):
+                    #print(i, j, facts[j].key, facts[j].value)
+                    if facts[j].key in condition:
+                        if isinstance(facts[j].value, float):
+                            condition = condition.replace("$" + facts[j].key, str(facts[j].value))
+                        elif isinstance(facts[j].value, str):
+                            condition = condition.replace("$" + facts[j].key, "'" + str(facts[j].value) + "'")
+                code = to_code(condition)
+                #print(code)
+                if "$"in code:
+                    continue
+                elif eval(code):
+                    facts.append(Fact(act_as_fact.key, act_as_fact.value))
+                    output += " правило удовлетворяет\n"
 
-        act = json.loads(arr_rules[i].acts)
-        if "температура" in act.keys():  # если в правиле действие-факт направлен на определение уровня температуры
-            t1, t2 = arr_rules[i].conditions
-            if my_temp >= t1 and my_temp < t2:
-                my_templvl = act["температура"]
-                # print("правило, удовлетворяющее цели: %s" % (arr_rules[i]))
-                output += " правило, удовлетворяющее цели: %s\n" % (arr_rules[i])
+        continue
 
-
-        elif "диагноз" in act.keys():  # если в правиле действие-факт направлен на определение диагноза
-            if arr_rules[
-                i].conditions != None:  # неопределеный диагноз не имеет условий-фактов !по приоритету он в самом конце!
-                start = json.loads(arr_rules[i].conditions)["начало"]
-                templvl = json.loads(arr_rules[i].conditions)["температура"]
-                if start == my_start and templvl == my_templvl:
-                    diag = act["диагноз"]
-                    # print(" правило, удовлетворяющее цели: %s" % (arr_rules[i]))
-                    output += " правило, удовлетворяющее цели: %s\n" % (arr_rules[i])
-                    break
-            elif arr_rules[
-                i].conditions == None:
-                diag = act["диагноз"]
-                # print(" правило, удовлетворяющее цели: %s" % (arr_rules[i])) #неопределенный диагноз
-                output += " правило, удовлетворяющее цели: %s\n" % (arr_rules[i])
+    output += "\n###############\nСписок фактов:\n"
+    for fact in facts:
+        output += str(fact) + "\n"
 
     with open("output.txt", 'a+', encoding="utf-8") as file:
         file.write(output)
     file.close()
 
-    return "_____________________\nINPUT: начало %s, температура %s - %s\nOUTPUT: диагноз %s" % (
-        my_start, my_temp, my_templvl, diag)
 
+if __name__ == "__main__":
+    #create_my_rules()
+    # facts=input_data()
 
-#create_my_rules()
-print(solving("постепенное", 36.9))
-print(solving("нет", 36.9))
-print(solving("лихорадочное", 38.9))
-print(solving("постепенное", 37.9))
+    facts = [Fact("температура", 37.2), Fact("начало", "постепенное")]
+    solving(facts)
 
-"""
-def input_data(self):
-    print("Начало заболевания")
-    start = input()
-    print("Введите температуру")
-    temp = int(input())
-    return start, temp
-"""
+    facts = [Fact("температура", 34.2), Fact("начало", "постепенное")]
+    solving(facts)
+
+    facts = [Fact("температура", 37.2)]
+    solving(facts)
+
+    facts = [Fact("температура", 36.2)]
+    solving(facts)
+
+    facts = [Fact("температура", 39.5), Fact("начало", "лихорадочное")]
+    solving(facts)
